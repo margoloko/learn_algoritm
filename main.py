e290472fb2a633b9bae1cf2fe37884505b0378b0
Marina <margoloko@mail.ru>
2022-09-28 19:29:11 +0700

    2_sum

---
+++ main.py
@@ -0,0 +1,33 @@
+#Ускорение скользящего среднего
+from typing import List, Tuple
+
+
+def moving_average(arr):
+    pass
+
+#Наивный вариант решения 2-SUM
+def two_sum(arr, x):
+    for i in range(0, len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] + arr[j] == x:
+                return arr[i], arr[j]
+            return None, None
+
+
+#Вариант решения 2-SUM с сортировкой
+def two_sum_sort(arr, x):
+    arr.sort() #сортируем массив
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        curr_sum = arr[left] + arr[right]
+        if curr_sum == x:
+            return arr[left], arr[right]
+        if curr_sum < x:
+            left += 1
+        else:
+            right -= 1
+    return None, None
+        
+#Вариант решения 2-SUM с использованием доп.памяти
+def two_sum_memory(arr, x):
