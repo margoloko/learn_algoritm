10a3dc6c1e21ec448dd46951c0e1efc977af946f
Marina <margoloko@mail.ru>
2022-10-13 16:02:25 +0700

    B. Чётные и нечётные числа

--- main.py
+++ main.py
@@ -39,3 +39,22 @@ def two_sum_memory(arr, x):
         else:
             temp.add(i)
     return None, None
+
+
+# Представьте себе онлайн-игру для поездки в метро: игрок нажимает на кнопку, и на экране появляются три случайных числа. 
+# Если все три числа оказываются одной чётности, игрок выигрывает.
+# Напишите программу, которая по трём числам определяет, выиграл игрок или нет.
+def check_parity(a: int, b: int, c: int) -> bool:    
+    if a % 2 == b % 2 == c % 2:
+        result = True
+        return result
+        
+
+def print_result(result: bool) -> None:
+    if result:
+        print("WIN")
+    else:
+        print("FAIL")
+
+a, b, c = map(int, input().strip().split())
+print_result(check_parity(a, b, c))
