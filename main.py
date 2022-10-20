bb7d162aa90e3616374c87afaecc33a4e104ec7c
Marina <margoloko@mail.ru>
2022-10-20 16:14:03 +0700

    add file final_1

--- main.py
+++ main.py
@@ -58,3 +58,26 @@ def print_result(result: bool) -> None:
 
 a, b, c = map(int, input().strip().split())
 print_result(check_parity(a, b, c))
+
+
+# Дана матрица. Нужно написать функцию, которая для элемента возвращает всех его соседей.
+# Соседним считается элемент, находящийся от текущего на одну ячейку влево, вправо, вверх или вниз.
+# Диагональные элементы соседними не считаются.
+from typing import List, Tuple
+
+def get_neighbours(matrix: List[List[int]], row: int, col: int) -> List[int]:
+    
+    pass
+
+def read_input() -> Tuple[List[List[int]], int, int]:
+    n = int(input())
+    m = int(input())
+    matrix = []
+    for i in range(n):
+        matrix.append(list(map(int, input().strip().split())))
+    row = int(input())
+    col = int(input())
+    return matrix, row, col
+
+matrix, row, col = read_input()
+print(" ".join(map(str, get_neighbours(matrix, row, col))))
